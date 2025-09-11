93a0873af893c5bb73c59f3dac454227cc70d554
Diego Ferreira L.G.Oliveira <diegoferreira1964@gmail.com>
2025-09-10 21:37:17 -0300

    feat: menu interativo

---
+++ frente.js
@@ -0,0 +1,145 @@
+module.exports = {
+  "PURPURA - RENTABILIZAÇÃO": {
+    GERAL: { 1: "MIGRA PRÉ -> CONTROLE", 2: "UPGRADE CONTROLE" },
+    CAPTIVE: { 1: "NO CREDIT", 2: "NO DATA" },
+    "APP VIVO": {
+      1: "HEADER",
+      2: "TROCAR DE ASSINATURA",
+      3: "TROCAR DE ASSINATURA E2E",
+      4: "ABA PRA VOCÊ - MELHOR OFERTA",
+      5: "ABA PRA VOCÊ - ESPECIAIS PRA VOCÊ",
+      6: "ABA ASSINATURAS - CALLOUT",
+      7: "ABA ASSINATURAS - OFERTAS PRO SEU CELULAR",
+      8: "ABA LOJA - MELHOR OFERTA",
+      9: "ABA LOJA - PRO SEU CELULAR",
+      10: "ABA LOJA - CATEGORIA - MELHOR OFERTA",
+      11: "ABA LOJA - CATEGORIA - OUTRAS OFERTAS",
+      12: "ABA LOJA - CATEGORIA - PARCERIAS",
+      13: "ABA LOJA - CATEGORIA - VALE TUDO",
+      14: "ABA RECARGA - BANNER",
+      15: "ÁREA ABERTA - BANNER",
+      16: "ÁREA ABERTA - MENU"
+    },
+    CLM: {
+      1: "CRM - CAPTIVE URA TRANSF - SMS",
+      2: "CRM - CAPTIVE NC - SMS",
+      3: "CRM - CAPTIVE URA 7000 - SMS",
+      4: "CRM - BARRADOS - SMS",
+      5: "CRM - BAU - PUSH",
+      6: "CRM - BAU - SMS",
+      7: "CRM - BAU - RCS",
+      8: "CRM - CARRINHO - SMS",
+      9: "CRM - CONSULTA SALDO - SMS",
+      10: "CRM - REPIQUE BEMOBI - SMS",
+      11: "CRM - RTDM - SATPUSH",
+      12: "CRM - RTDM - SMS - 80",
+      13: "CRM - RTDM - SMS - 100",
+      14: "CRM - RTDM - SMS - LIMIAR",
+      15: "CRM - WELCOME - SMS",
+      16: "CRM - OPT OUT - SMS"
+    },
+    "DEMAIS CANAIS": { 1: "PORTAL" },
+    AURA: { 1: "AURA MIGRA PRÉ CONTROLE", 2: "AURA UPGRADE CONTROLE" },
+    "CONTROLE CARTÃO": { 1: "CONTROLE CARTÃO" }
+  },
+
+  "ALTO VALOR - RENTABILIZAÇÃO": {
+    GERAL: {
+      1: "MIGRA CONTROLE >> PÓS",
+      2: "UPGRADE PÓS"
+    },
+    CAPTIVE: {
+      1: "NO CREDIT",
+      2: "NO DATA"
+    },
+    "APP VIVO": {
+      1: "HEADER",
+      2: "TROCAR DE ASSINATURA",
+      3: "TROCAR DE ASSINATURA E2E",
+      4: "ABA PRA VOCÊ - MELHOR OFERTA",
+      5: "ABA PRA VOCÊ - ESPECIAIS PRA VOCÊ",
+      6: "ABA ASSINATURAS - CALLOUT",
+      7: "ABA ASSINATURAS - OFERTAS PRO SEU CELULAR",
+      8: "ABA LOJA - MELHOR OFERTA",
+      9: "ABA LOJA - PRO SEU CELULAR",
+      10: "ABA LOJA - CATEGORIA - MELHOR OFERTA",
+      11: "ABA LOJA - CATEGORIA - PARCERIAS",
+      12: "ABA LOJA - CATEGORIA - VALE TUDO",
+      13: "ÁREA ABERTA"
+    },
+    CLM: {
+      1: "CLM BAU SMS",
+      2: "CLM RTDM SMS 80",
+      3: "CLM RTDM SMS 100",
+      4: "CLM ABANDONO MENU TROCA SMS",
+      5: "BAU - E-MAIL",
+      6: "BAU - RCS",
+      7: "BAU - PUSH",
+      8: "CARRINHO ABANDONADO",
+      9: "BAU SMS - OPTOUT",
+      10: "BAU SMS - FTTH",
+      11: "SMS NO DATA E NO CREDIT",
+      12: "SMS TRAFEGO SUJO"
+    },
+    PORTAL: {
+      1: "PÓS HUB",
+      2: "PÓSCARD"
+    },
+    AURA: {
+      1: "MIGRA CONTROLE PÓS"
+    }
+  },
+
+  "PURPURA - AQUISIÇÃO": {
+    CANAIS: {
+      1: "LOJA ONLINE ALTAS",
+      2: "LOJA ONLINE PORTABILIDADE",
+      3: "AURA ALTAS",
+      4: "AURA PORTABILIDADE"
+    },
+    "E-SIM": { 1: "E-SIM CONTROLE" },
+    "APP VIVO": {
+      1: "ÁREA ABERTA MENU",
+      2: "ÁREA ABERTA BANNER",
+      3: "MELHOR OFERTA",
+      4: "ESPECIAIS PARA VOCÊ",
+      5: "LOJA ASSINAR",
+      6: "APP VIVO DEMAIS"
+    },
+    "DEMAIS CANAIS": {
+      1: "PORTAL",
+      2: "CLM",
+      3: "FACEBOOK",
+      4: "SEARCH",
+      5: "DISPLAY",
+      6: "MÍDIA DEMAIS",
+      7: "OUTROS"
+    }
+  },
+
+  "ALTO VALOR - AQUISIÇÃO": {
+    CANAIS: {
+      1: "LOJA ONLINE ALTAS",
+      2: "UPGRADE PÓS",
+      3: "AURA ALTAS",
+      4: "AURA PORTABILIDADE"
+    },
+    "E-SIM": { 1: "E-SIM PÓS" },
+    "APP VIVO": {
+      1: "ÁREA ABERTA MENU",
+      2: "MELHOR OFERTA",
+      3: "ESPECIAIS PARA VOCÊ",
+      4: "LOJA ASSINAR",
+      5: "APP VIVO DEMAIS"
+    },
+    "DEMAIS CANAIS": {
+      1: "PORTAL",
+      2: "CLM",
+      3: "FACEBOOK",
+      4: "SEARCH",
+      5: "DISPLAY",
+      6: "MÍDIA DEMAIS",
+      7: "OUTROS"
+    }
+  }
+};
